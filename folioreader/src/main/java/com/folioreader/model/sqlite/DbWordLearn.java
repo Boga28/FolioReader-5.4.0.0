611017989a707445da3180b0fb5864f1d4572909
General Team <46902785+Boga28@users.noreply.github.com>
2020-05-02 02:36:40 +0300

    Update DbWordLearn.java

--- folioreader/src/main/java/com/folioreader/model/sqlite/DbWordLearn.java
+++ folioreader/src/main/java/com/folioreader/model/sqlite/DbWordLearn.java
@@ -12,7 +12,7 @@ public class DbWordLearn extends SQLiteOpenHelper {
 
     private static final String TAG = DbWordLearn.class.getSimpleName();
 
-    public static final int DATABASE_VERSION =1;
+    public static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "wordLearn";
     private static final String TABLE_NAME = "wordLearnData";
     private static final String KEY_ID = "id";
@@ -26,15 +26,15 @@ public class DbWordLearn extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-            String CREATE_TABLE = " CREATE TABLE " +" "+ TABLE_NAME + " ( "
-                    + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                    + KEY_WORD + " TEXT, "
-                    + KEY_LEARN + " INTEGER DEFAULT 0, "
-                    + KEY_LEARNED + " INTEGER DEFAULT 0 "
-                    +" )";
+        String CREATE_TABLE = " CREATE TABLE " + " " + TABLE_NAME + " ( "
+                + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + KEY_WORD + " TEXT, "
+                + KEY_LEARN + " INTEGER DEFAULT 0, "
+                + KEY_LEARNED + " INTEGER DEFAULT 0 "
+                + " )";
 
-            sqLiteDatabase.execSQL(CREATE_TABLE);
-            Log.d("Veritabanı Kuruldu", "Tablo Oluşturuldu.");
+        sqLiteDatabase.execSQL(CREATE_TABLE);
+        Log.d("Veritabanı Kuruldu", "Tablo Oluşturuldu.");
     }
 
     @Override
@@ -45,29 +45,36 @@ public class DbWordLearn extends SQLiteOpenHelper {
 
     public void addWord(Context context, String wordd, String learn, String learned) {
         SQLiteDatabase db = this.getWritableDatabase();
+        Boolean exist= IsProductExist(wordd);
         ContentValues values = new ContentValues();
-        Cursor kayitlar=null;
-        String query="SELECT * FROM "+TABLE_NAME+" WHERE word="+wordd+" LIMIT 1";
-        try {
-            kayitlar = db.rawQuery(query, null);
-        }catch (Exception e){ Toast.makeText(context, "Hata var Ulo", Toast.LENGTH_SHORT).show();}
-        assert kayitlar != null;
-        if(kayitlar.getPosition() >= 0 ){
-            String idd = kayitlar.getString(kayitlar.getColumnIndex("id"));
-            values.put(KEY_LEARN,learn);
-            values.put(KEY_LEARNED,learned);
-            db.update(TABLE_NAME,values,"id = " + idd,null);
-            Toast.makeText(context, "Kelime zaten mevcut ve güncellendi: " + wordd + " :  " + learn + "  :  " + learned, Toast.LENGTH_SHORT).show();
-        }else{
-            values.put(KEY_WORD, wordd);
-            values.put(KEY_LEARN, learn);
-            values.put(KEY_LEARNED, learned);
-            db.insert(TABLE_NAME, null, values);
-            Toast.makeText(context, "Kelime oluşturuldu:  " + wordd + " :  " + learn + "  :  " + learned, Toast.LENGTH_LONG).show();
-        }
+        values.put(KEY_WORD, wordd);
+        values.put(KEY_LEARN, learn);
+        values.put(KEY_LEARNED, learned);
+        db.insert(TABLE_NAME, null, values);
+        Toast.makeText(context, "Kelime oluşturuldu:  " + wordd + " :  " + learn + "  :  " + learned, Toast.LENGTH_LONG).show();
+
         db.close();
     }
 
+    public boolean IsProductExist(String words) {
+        Cursor DB_cursor = null;
+        SQLiteDatabase sqlDB = this.getWritableDatabase();
+        try {
+            String DB_query = "SELECT * FROM " +TABLE_NAME+ " WHERE word=" + words + ";";
+            DB_cursor = sqlDB.rawQuery(DB_query, null);
+
+            if (DB_cursor.moveToFirst()) {
+                DB_cursor.close();
+                return true;
+            } else {
+                DB_cursor.close();
+                return false;
+            }
+        } catch (Exception ex) {
+            DB_cursor.close();
+            return false;
+        }
+    }
 
 
 }
